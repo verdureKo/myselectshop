1afddd0c55184ae7287b6fb186ff3afbf852bf4f
VerdureKo <verdure.ko@gmail.com>
2023-07-26 21:04:45 +0900

    Controller 테스트

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling	// 스케줄러 작동 Annotation
-@EnableJpaAuditing	// 시간 자동변경 Annotation
 @SpringBootApplication
 public class MyselectshopApplication {
 
