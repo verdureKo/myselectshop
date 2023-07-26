6bf280fa24f271f7aec0fa5ab30dc5af97302acb
VerdureKo <verdure.ko@gmail.com>
2023-07-26 21:08:22 +0900

    ExceptionHandler 적용

--- src/main/java/com/sparta/myselectshop/exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.myselectshop.exception;
-
-public class ProductNotFoundException extends RuntimeException{
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/sparta/myselectshop/service/FolderService.java
+++ src/main/java/com/sparta/myselectshop/service/FolderService.java
@@ -9,45 +9,28 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class FolderService {
 
     private final FolderRepository folderRepository;
 
-    // 로그인한 회원에 폴더들 등록
     public void addFolders(List<String> folderNames, User user) {
-
-        // 입력으로 들어온 폴더 이름을 기준으로, 회원이 이미 생성한 폴더들을 조회합니다.
         List<Folder> existFolderList = folderRepository.findAllByUserAndNameIn(user, folderNames);
-
         List<Folder> folderList = new ArrayList<>();
 
         for (String folderName : folderNames) {
-            // 이미 생성한 폴더가 아닌 경우만 폴더 생성
-            if (!isExistFolderName(folderName, existFolderList)) {
+            if(!isExistFolderName(folderName, existFolderList)){
                 Folder folder = new Folder(folderName, user);
                 folderList.add(folder);
-            } else {
-                throw new IllegalArgumentException("폴더명이 중복되었습니다.");
+            }else{
+                throw new IllegalArgumentException("중복된 폴더명을 제거해주세요 폴더명 : "+folderName);
             }
         }
 
         folderRepository.saveAll(folderList);
     }
 
-    private Boolean isExistFolderName(String folderName, List<Folder> existFolderList) {
-        // 기존 폴더 리스트에서 folder name 이 있는지?
-        for (Folder existFolder : existFolderList) {
-            if(folderName.equals(existFolder.getName())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // 로그인한 회원이 등록된 모든 폴더 조회
     public List<FolderResponseDto> getFolders(User user) {
         List<Folder> folderList = folderRepository.findAllByUser(user);
         List<FolderResponseDto> responseDtoList = new ArrayList<>();
@@ -55,8 +38,15 @@ public class FolderService {
         for (Folder folder : folderList) {
             responseDtoList.add(new FolderResponseDto(folder));
         }
-
         return responseDtoList;
     }
-}
+    private boolean isExistFolderName(String folderName, List<Folder> existFolderList) {
+        for (Folder existFolder : existFolderList) {
+            if(existFolder.getName().equals(folderName)){
+                return true;
+            }
+        }
+        return false;
+    }
 
+}
