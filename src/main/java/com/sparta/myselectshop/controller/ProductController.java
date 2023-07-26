5a2cee6a8d671e42060313614280251f16836679
VerdureKo <verdure.ko@gmail.com>
2023-07-26 21:06:30 +0900

    Top5 회원 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -3,6 +3,9 @@ package com.sparta.myselectshop.controller;
 import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.entity.ApiUseTime;
+import com.sparta.myselectshop.entity.User;
+import com.sparta.myselectshop.repository.ApiUseTimeRepository;
 import com.sparta.myselectshop.security.UserDetailsImpl;
 import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -17,47 +20,78 @@ import java.util.List;
 @RequestMapping("/api")
 public class ProductController {
 
-    private  final  ProductService productService;
+    private final ProductService productService;
+    private final ApiUseTimeRepository apiUseTimeRepository;
 
-    // 관심 상품 등록하기
     @PostMapping("/products")
     public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        // 응답 보내기
-        return productService.createProduct(requestDto, userDetails.getUser());
+        // 측정 시작 시간
+        long startTime = System.currentTimeMillis();
+
+        try {
+            // 응답 보내기
+            return productService.createProduct(requestDto, userDetails.getUser());
+        } finally {
+            // 측정 종료 시간
+            long endTime = System.currentTimeMillis();
+            // 수행시간 = 종료 시간 - 시작 시간
+            long runTime = endTime - startTime;
+
+            // 로그인 회원 정보
+            User loginUser = userDetails.getUser();
+
+            // API 사용시간 및 DB 에 기록
+            ApiUseTime apiUseTime = apiUseTimeRepository.findByUser(loginUser)
+                    .orElse(null);
+            if (apiUseTime == null) {
+                // 로그인 회원의 기록이 없으면
+                apiUseTime = new ApiUseTime(loginUser, runTime);
+            } else {
+                // 로그인 회원의 기록이 이미 있으면
+                apiUseTime.addUseTime(runTime);
+            }
+
+            System.out.println("[API Use Time] Username: " + loginUser.getUsername() + ", Total Time: " + apiUseTime.getTotalTime() + " ms");
+            apiUseTimeRepository.save(apiUseTime);
+        }
     }
 
-    // 상품에 폴더 추가
-    @PostMapping("/products/{productId}/folder")
-    public void addFolder(
-            @PathVariable Long productId,
-            @RequestParam Long folderId,
-            @AuthenticationPrincipal UserDetailsImpl userDetails
-    ) {
-        productService.addFolder(productId, folderId, userDetails.getUser());
+    @PutMapping("/products/{id}")
+    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto){
+        return productService.updateProduct(id, requestDto);
     }
 
-    // 관심 상품 조회하기
     @GetMapping("/products")
     public Page<ProductResponseDto> getProducts(
             @RequestParam("page") int page,
             @RequestParam("size") int size,
             @RequestParam("sortBy") String sortBy,
             @RequestParam("isAsc") boolean isAsc,
-            @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        // 응답 보내기
-        return productService.getProducts(userDetails.getUser(),  page-1, size, sortBy, isAsc);
+
+            @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return productService.getProducts(userDetails.getUser(),
+                page-1, size, sortBy, isAsc
+                );
     }
 
-    // 회원이 등록한 폴더 내 모든 상품 조회
+    @PostMapping("/products/{productId}/folder")
+    public void addFolder(
+            @PathVariable Long productId,
+            @RequestParam Long folderId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails){
+        productService.addFolder(productId,folderId,userDetails.getUser());
+    }
+
     @GetMapping("/folders/{folderId}/products")
     public Page<ProductResponseDto> getProductsInFolder(
             @PathVariable Long folderId,
-            @RequestParam int page,
-            @RequestParam int size,
-            @RequestParam String sortBy,
-            @RequestParam boolean isAsc,
-            @AuthenticationPrincipal UserDetailsImpl userDetails
-    ) {
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc,
+
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
         return productService.getProductsInFolder(
                 folderId,
                 page-1,
@@ -66,18 +100,6 @@ public class ProductController {
                 isAsc,
                 userDetails.getUser()
         );
-    }
-
-    @GetMapping("/admin/products")
-    public List<ProductResponseDto> getAllProducts() {
-        return productService.getAllProducts();
-    }
-
-    // 관심 상품 희망 최저가 등록하기
-    @PutMapping("/products/{id}")
-    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
-        // 응답 보내기
-        return productService.updateProduct(id, requestDto);
     }
 
 }
